59f4b3529f27ad074c9a97ad3c0f0a26567ddc89
Krister Riska <31847374+kristerriska@users.noreply.github.com>
2018-05-25 08:30:26 +0300

    Add files via upload
    Parandused

--- extension/popup.js
+++ extension/popup.js
@@ -1,14 +1,17 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("reddit").addEventListener("click", reddit);
-    document.getElementById("vikipeedia").addEventListener("click", vikipeedia);
+
+	document.addEventListener("DOMContentLoaded", function(){
+    	document.getElementById("reddit").addEventListener("click", reddit);
+    	document.getElementById("vikipeedia").addEventListener("click", vikipeedia);
 });
-function reddit(){
+	
+	function reddit(){
 		var d = document.getElementById("navbar-query").value;
-		var webpage = "https://www.reddit.com/search?q="+ d;
-		window.open(webpage);
+			var webpage = "https://www.reddit.com/search?q="+ d;
+				window.open(webpage);
 	}
+
 	function vikipeedia(){
 		var d = document.getElementById("navbar-query").value;
-		var webpage = "https://et.wikipedia.org/w/index.php?search="+ d +"&title=Eri:Otsimine&go=Mine&searchToken=pfw3cl6c55bzlll5jq28b6ny";
-		window.open(webpage);
+			var webpage = "https://et.wikipedia.org/w/index.php?search="+ d +"&title=Eri:Otsimine&go=Mine&searchToken=pfw3cl6c55bzlll5jq28b6ny";
+				window.open(webpage);
 	}
